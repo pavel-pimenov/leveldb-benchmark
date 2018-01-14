18d0138197f2418c52545b6efa619a3b62211a4b
Pavel Pimenov <pavel.pimenov@gmail.com>
2018-01-14 21:24:34 +0300

    * Add calc count record

--- src/leveldb-clone.cpp
+++ src/leveldb-clone.cpp
@@ -6,40 +6,54 @@
 int main() {
     leveldb::DB* db;
     leveldb::Options options;
-    options.create_if_missing = true;
-    leveldb::Status status = leveldb::DB::Open(options,"/tmp/testdb", &db);
+    options.create_if_missing = false;
+    leveldb::Status status = leveldb::DB::Open(options,"tth-history.leveldb", &db);
     assert(status.ok());
 
-//write key1,value1
-    std::string key="key";
-    std::string value = "value";
+    /*
+    //write key1,value1
+        std::string key="key";
+        std::string value = "value";
 
-    status = db->Put(leveldb::WriteOptions(), key,value);
-    assert(status.ok());
+        status = db->Put(leveldb::WriteOptions(), key,value);
+        assert(status.ok());
 
-    status = db->Get(leveldb::ReadOptions(), key, &value);
-    assert(status.ok());
-    std::cout<<value<<std::endl;
-    std::string key2 = "key2";
-
+        status = db->Get(leveldb::ReadOptions(), key, &value);
+        assert(status.ok());
+        std::cout<<value<<std::endl;
+        std::string key2 = "key2";
+    */
 //move the value under key to key2
+    /*
 
-    status = db->Put(leveldb::WriteOptions(),key2,value);
-    assert(status.ok());
-    status = db->Delete(leveldb::WriteOptions(), key);
+        status = db->Put(leveldb::WriteOptions(),key2,value);
+        assert(status.ok());
+        status = db->Delete(leveldb::WriteOptions(), key);
 
-    assert(status.ok());
+        assert(status.ok());
 
-    status = db->Get(leveldb::ReadOptions(),key2, &value);
+        status = db->Get(leveldb::ReadOptions(),key2, &value);
 
-    assert(status.ok());
-    std::cout<<key2<<"==="<<value<<std::endl;
+        assert(status.ok());
+        std::cout<<key2<<"==="<<value<<std::endl;
 
-    status = db->Get(leveldb::ReadOptions(),key, &value);
+        status = db->Get(leveldb::ReadOptions(),key, &value);
 
-    if(!status.ok()) std::cerr <<key<<" error   " << status.ToString()<<std::endl;
-    else std::cout<<key<<"==="<<value<<std::endl;
-
+        if(!status.ok()) std::cerr <<key<<" error   " << status.ToString()<<std::endl;
+        else std::cout<<key<<"==="<<value<<std::endl;
+    */
+    unsigned count = 0;
+    leveldb::Iterator* it = db->NewIterator(leveldb::ReadOptions());
+    for (it->SeekToFirst(); it->Valid(); it->Next()) {
+        //std::cout << it->key().ToString() << ": "  << it->value().ToString() << std::endl;
+        if(++count % 1000 == 0)
+        {
+            std::cout << "i = " << count << std::endl;
+        }
+    }
+    assert(it->status().ok());  // Check for any errors found during the scan
+    delete it;
+    std::cout << "count = " << count << std::endl;
     delete db;
     return 0;
 }
