d3818c2543485f9de144dd058cb11629fcdf3f3c
pavel pimenov <pavel.pimenov@gmail.com>
2018-01-17 09:58:46 +0300

    * Add class CElapsedSeconds

--- src/build-visualc-2015.bat
+++ src/build-visualc-2015.bat
@@ -5,7 +5,7 @@
 
 @rem https://msdn.microsoft.com/en-us/library/fwkeyyhe.aspx
 
-cl /I ".\leveldb" /I ".\leveldb\include" /I ".\leveldb\util" /I ".\leveldb\table" /I ".\leveldb\db" /I ".\leveldb\port\win" /D _CONSOLE /D LEVELDB_PLATFORM_WINDOWS /W4 /O2 leveldb-clone.cpp @leveldb-files -Felveldb-clone.exe 
+cl /MT /I ".\leveldb" /I ".\leveldb\include" /I ".\leveldb\util" /I ".\leveldb\table" /I ".\leveldb\db" /I ".\leveldb\port\win" /D _CONSOLE /D LEVELDB_PLATFORM_WINDOWS /W4 /O2 leveldb-clone.cpp @leveldb-files -Felveldb-clone.exe
 @del *.obj
 
 @rem /I \leveldb 
--- src/leveldb-clone.cpp
+++ src/leveldb-clone.cpp
@@ -1,46 +1,69 @@
-#include <assert.h>
+#define WINVER _WIN32_WINNT_WINXP
+#define _WIN32_WINNT _WIN32_WINNT_WINXP
+#define NTDDI_VERSION NTDDI_WINXP
+
 #include <string.h>
 #include "leveldb/db.h"
 #include <iostream>
+#include <chrono>
+
+class CElapsedSeconds
+{
+public:
+    std::chrono::time_point<std::chrono::system_clock> m_start;
+    std::string m_caption;
+public:
+    CElapsedSeconds(const std::string& p_caption) : m_caption(p_caption)
+    {
+        m_start = std::chrono::system_clock::now();
+    }
+    ~CElapsedSeconds()
+    {
+        const auto l_end = std::chrono::system_clock::now();
+        std::cout << m_caption << "[ duration: " << std::chrono::duration <double, std::milli> (l_end - m_start).count() << " ms ]" << std::endl;
+    }
+};
 
 int main() {
     int result = 0;
     leveldb::DB* db_old = NULL;
     leveldb::Options options;
     options.create_if_missing = true;
-    const char* name_db_old = "tth-history-old.leveldb";	
+    const char* name_db_old = "tth-history-old.leveldb";
     leveldb::Status status = leveldb::DB::Open(options,name_db_old, &db_old);
     if(!status.ok())
-	{
-	        std::cout << "Error open "<< name_db_old << " message: "<< status.ToString() << std::endl;
-		return -1;
-	}	
+    {
+        std::cout << "Error open "<< name_db_old << " message: "<< status.ToString() << std::endl;
+        return -1;
+    }
 
     leveldb::DB* db_new = NULL;
     leveldb::Options options_new;
     options_new.create_if_missing = true;
-    const char* name_db_new = "tth-history-new.leveldb";	
+    const char* name_db_new = "tth-history-new.leveldb";
     status = leveldb::DB::Open(options_new,name_db_new, &db_new);
     if(!status.ok())
-	{
-	        std::cout << "Error open "<< name_db_new << " message: "<< status.ToString() << std::endl;
-		return -1;
-	}	
+    {
+        std::cout << "Error open "<< name_db_new << " message: "<< status.ToString() << std::endl;
+        return -1;
+    }
     std::string key;
     std::string value;
-    for(int i = 0; i< 100000; i++)	
-	{
-		char buf[100];
-		snprintf(buf, sizeof(buf), "key-%d-key-key-key-key-key-key-key-key-%d", i, i);
-		key = buf;
-		value = key+"~"+key;
-        	status = db_old->Put(leveldb::WriteOptions(), key,value);
-		if(!status.ok())
-		{
-	        	std::cout << "Error Put " << i << " message: "<< status.ToString() << std::endl;
-		}
-	}
-     std::cout << "Fill  100000 !" << std::endl;
+    {
+        CElapsedSeconds l_time("Create test leveldb - 1000000 record");
+        for(int i = 0; i< 1000000; i++)
+        {
+            char buf[100];
+            snprintf(buf, sizeof(buf), "key-%d-key-key-key-key-key-key-key-key-%d", i, i);
+            key = buf;
+            value = key+"~"+key;
+            status = db_old->Put(leveldb::WriteOptions(), key,value);
+            if(!status.ok())
+            {
+                std::cout << "Error Put " << i << " message: "<< status.ToString() << std::endl;
+            }
+        }
+    }
     /*
     //write key1,value1
         std::string key="key";
@@ -73,28 +96,31 @@ int main() {
         if(!status.ok()) std::cerr <<key<<" error   " << status.ToString()<<std::endl;
         else std::cout<<key<<"==="<<value<<std::endl;
     */
-    unsigned count = 0;
-    leveldb::Iterator* it = db_old->NewIterator(leveldb::ReadOptions());
-    int i = 0;
-    for (it->SeekToFirst(); it->Valid(); it->Next()) {
-       	status = db_new->Put(leveldb::WriteOptions(), it->key(),it->value());
-       	++i;
-	if(!status.ok())
-	{
-        	std::cout << "Error Put(new)" << i << " message: "<< status.ToString() << std::endl;
-	}
-        if(++count % 1000 == 0)
-        {
-            std::cout << "count = " << count << std::endl;
+    {
+        CElapsedSeconds l_time("Clone 1000000 record");
+        unsigned count = 0;
+        leveldb::Iterator* it = db_old->NewIterator(leveldb::ReadOptions());
+        int i = 0;
+        for (it->SeekToFirst(); it->Valid(); it->Next()) {
+            status = db_new->Put(leveldb::WriteOptions(), it->key(),it->value());
+            ++i;
+            if(!status.ok())
+            {
+                std::cout << "Error Put(new)" << i << " message: "<< status.ToString() << std::endl;
+            }
+            if(++count % 100000 == 0)
+            {
+                std::cout << "count = " << count << std::endl;
+            }
         }
+        if(!it->status().ok())  // Check for any errors found during the scan
+        {
+            std::cout << "Error iterator: "<< it->status().ToString() << std::endl;
+            result = -1;
+        }
+        delete it;
+        std::cout << "Count record = " << count << std::endl;
     }
-    if(!it->status().ok())  // Check for any errors found during the scan
-	{
-	        std::cout << "Error iterator: "<< it->status().ToString() << std::endl;
-		result = -1;
-	}
-    delete it;
-    std::cout << "Count record = " << count << std::endl;
     delete db_old;
     delete db_new;
     return result;
