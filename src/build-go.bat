56d50c84578ed24f964ed42268c167f5fcdd2349
pavel.pimenov <pavel.pimenov@gmail.com>
2018-01-18 06:19:04 +0300

    * Rename leveldb-clone.go -> leveldb-clone-go.go

--- src/build-go.bat
+++ src/build-go.bat
@@ -1,6 +1,6 @@
 @del leveldb-clone.exe
 @rem go get github.com/syndtr/goleveldb/leveldb
-gofmt -w leveldb-clone.go
+gofmt -w leveldb-clone-go.go
 SET GOARCH=386
-go build leveldb-clone.go
+go build leveldb-clone-go.go
 rem go build -ldflags "-s -w" leveldb-clone.go
