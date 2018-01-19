425abda8b06c06d1b02c80f0ff75a6907d8a8872
pavel.pimenov <pavel.pimenov@gmail.com>
2018-01-19 19:26:09 +0300

    * Rename leveldb-clone -> leveldb-clone-cpp

--- src/build-visualc-2015.bat
+++ src/build-visualc-2015.bat
@@ -1,13 +1,13 @@
 @del leveldb-clone.exe
 @del *.obj
-AStyle.exe leveldb-clone.cpp
+AStyle.exe leveldb-clone-cpp.cpp
 @del *.cpp.orig
 
 @call "%VS140COMNTOOLS%\..\..\VC\bin\vcvars32.bat"
 
 @rem https://msdn.microsoft.com/en-us/library/fwkeyyhe.aspx
 
-cl /MT /I ".\leveldb" /I ".\leveldb\include" /I ".\leveldb\util" /I ".\leveldb\table" /I ".\leveldb\db" /I ".\leveldb\port\win" /D _CONSOLE /D LEVELDB_PLATFORM_WINDOWS /D WINVER=0x0501 /D WIN32_LEAN_AND_MEAN /D NOMINMAX /D _CRT_SECURE_NO_WARNINGS /D _CRT_NONSTDC_NO_WARNINGS /W3 /Oi /EHsc leveldb-clone.cpp @leveldb-files -Feleveldb-clone.exe
+cl /MT /I ".\leveldb" /I ".\leveldb\include" /I ".\leveldb\util" /I ".\leveldb\table" /I ".\leveldb\db" /I ".\leveldb\port\win" /D _CONSOLE /D LEVELDB_PLATFORM_WINDOWS /D WINVER=0x0501 /D WIN32_LEAN_AND_MEAN /D NOMINMAX /D _CRT_SECURE_NO_WARNINGS /D _CRT_NONSTDC_NO_WARNINGS /W3 /Oi /EHsc leveldb-clone-cpp.cpp @leveldb-files -Feleveldb-clone-cpp.exe
 
 @del *.obj
 
